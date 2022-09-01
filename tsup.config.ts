57cf994bcdb5d3a0917d0a797243a6944bac6323
eiwos <mahmoudi.otman@gmail.com>
2022-09-01 21:58:00 +0200

    Initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, Options } from 'tsup';
+
+const options: Options = {
+  minify: true,
+  minifyWhitespace: true,
+  minifyIdentifiers: true,
+  minifySyntax: true,
+  keepNames: false,
+  outDir: './dist/back-end/',
+  target: 'node16',
+  format: 'cjs',
+  clean: true,
+  platform: 'node',
+};
+
+export default defineConfig(options);
