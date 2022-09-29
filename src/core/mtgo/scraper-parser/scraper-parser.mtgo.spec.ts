9d72c67fcacd6d239b7614217c9ea1f551fc51d0
eiwos <mahmoudi.otman@gmail.com>
2022-09-29 21:55:45 +0200

    Test updated for link builder

---
+++ src/core/mtgo/scraper-parser/scraper-parser.mtgo.spec.ts
@@ -0,0 +1,25 @@
+import { getDataFromUrl, parseMtgo, scraperParserRUN } from './scraper-parser.mtgo';
+
+describe('Testing scraper-parser.mtgo.ts functions', () => {
+  const realLinkStd = 'https://magic.wizards.com/en/articles/archive/mtgo-standings/standard-league-2022-09-29';
+  const fakeLinkStd = 'https://magic.wizards.com/en/articles/archive/mtgo-standings/standard-league-2020-09-29';
+  const expectData = getDataFromUrl(realLinkStd);
+  const expectNull = getDataFromUrl(fakeLinkStd);
+
+  test('Test getDataFromUrl', async () => {
+    const rData = await expectData;
+
+    expect(rData !== null).toBeTruthy();
+  });
+
+  test('Test parseMtgo', async () => {
+    const rData = await expectData;
+    const rNull = await expectNull;
+    // @ts-ignore
+    const pData = await parseMtgo(rData.name, rData.data);
+    // @ts-ignore
+    const pNull = await parseMtgo(rNull.name, rNull.data);
+
+    expect(pData !== null).toBeTruthy();
+  });
+});
