b4761c01d7a9a2ee0308b159c1649fc0a32fcee2
eiwos <mahmoudi.otman@gmail.com>
2022-09-29 20:24:52 +0200

    New update for npm and suppression of unused package (puppeteer)

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 /**
  * Export for libs usability
  */
-export { ScraperParserMtgo } from './core/mtgo/scraper-parser/scraper-parser.mtgo';
-export { LinkBuilderMtgo } from './core/mtgo/link-builder/link-builder.mtgo';
+export { getDataFromUrl, parseMtgo, scraperParserRUN } from './core/mtgo/scraper-parser/scraper-parser.mtgo';
+export { linkGenerator, checkLink, checkArrayOfLinks, linkBuilderRUN } from './core/mtgo/link-builder/link-builder.mtgo';
 export { filtering } from './core/filter/filter.core';
 export {
-  Card,
-  Filter,
-  Deck,
-  Tournament,
-  ConfigurationLinker,
-  Format,
-  Platform,
-  Organizer,
-  LevelOfPlay
-} from './core/common.core';
+  ICard,
+  IFilter,
+  IDeck,
+  ITournament,
+  IConfigurationLinker,
+  TFormat,
+  TPlatform,
+  TOrganizer,
+  TLevelOfPlay
+} from './core/types.core';
 
 /**
  * Tournament scraping: Around 1 second
