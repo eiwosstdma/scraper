1444d77fd9eee3d03252eef5b353c613766a77a7
eiwos <mahmoudi.otman@gmail.com>
2022-09-29 20:27:50 +0200

    New update for npm and suppression of unused package (puppeteer)

--- src/core/utilities.core.ts
+++ src/core/utilities.core.ts
@@ -11,7 +11,21 @@ export const generate12LString = () => {
   return randomBytes(6).toString('hex');
 };
 
-/**
- *
- */
 export const sleepUntil = promisify(setTimeout);
+
+export const typeGuard = <T>(unknownObj: unknown, knownObj: T, howMuchItNeedsToCompare: number): T | null => {
+  if (!(unknownObj instanceof Object)) return null;
+  if (!(knownObj instanceof Object)) throw new TypeError('typeGuard Function: Should be of type object');
+
+  const propsOfKnown = Object.keys(knownObj);
+  const propsOfUnknown = Object.keys(unknownObj);
+  let isFoundEqually = 0;
+
+  for (const propUnknown of propsOfUnknown) {
+    const isFound = propsOfKnown.find(prop => prop === propUnknown);
+    if (isFound) isFoundEqually++;
+  }
+
+  if (isFoundEqually >= howMuchItNeedsToCompare) return unknownObj as T;
+  else return null;
+};
