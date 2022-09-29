21d9a82fbaf5a80c8d07316014c4a171e036e909
eiwos <mahmoudi.otman@gmail.com>
2022-09-29 20:22:33 +0200

    Every time renamed

---
+++ src/core/types.core.ts
@@ -0,0 +1,60 @@
+/**
+ * Types for the Application
+ */
+export interface ICard {
+  quantity: number;
+  name: string;
+  linkId?: string;
+}
+
+export interface IFilter {
+  format: TFormat;
+  name: string;
+  includes: Array<string>;
+  excludes: Array<string>;
+}
+
+export interface IDeck {
+  id?: number;
+  tournamentName: string;
+  playedAt: string;
+  postedAt?: string;
+  name: string;
+  subId: string;
+  owner: string;
+  format: TFormat;
+  main: Array<ICard>;
+  side: Array<ICard>;
+  rank?: number;
+  point?: number;
+  omwp?: string;
+  gwp?: string;
+  ogwp?: string;
+}
+
+export interface ITournament {
+  id?: number;
+  publicId: string;
+  name: string;
+  playedAt: string;
+  postedAt: string;
+  linkTo: string;
+  format: TFormat;
+  totalPlayer: number;
+  organizer: TOrganizer;
+  platform: TPlatform;
+  levelOfPlay: TLevelOfPlay;
+}
+
+export interface IConfigurationLinker {
+  wantedFormat: Array<TFormat>;
+  wantedLevel?: Array<TLevelOfPlay>;
+}
+
+export type TFormat = 'vintage' | 'legacy' | 'modern' | 'pioneer' | 'standard' | 'pauper';
+
+export type TPlatform = 'mtgo';
+
+export type TOrganizer = 'wizard';
+
+export type TLevelOfPlay = 'league' | 'preliminary' | 'challenge' | 'showcase-challenge' | 'super-qualifier' | 'premier';
